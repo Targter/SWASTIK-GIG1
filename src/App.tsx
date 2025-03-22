7418d1e064589493659e0a968e5e3684adf7284f
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-03-22 12:40:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
           
-          <h1 className="text-6xl text-white mb-12">SARPANCH EXCHANGE</h1>
+          <h1 className="md:text-6xl sm:text-4xl text-2xl text-white mb-12">SARPANCH EXCHANGE</h1>
           <p className="text-2xl text-white mb-12">WE OFFER BEST & GENUINE EXPERIENCE</p>
           {/* <div className="w-24 h-1 bg-white mb-12"></div> */}
 
