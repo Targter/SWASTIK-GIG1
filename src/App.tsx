8987a2e6dc6c8a155b3dd585e5e5f3a9c7f8d92a
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-04-05 14:31:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,12 +45,12 @@ function App() {
         }}
       >
 {/*         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
           
           <h1 className="md:text-6xl sm:text-4xl text-2xl text-white mb-12">SARPANCH EXCHANGE</h1>
           <p className="text-2xl text-white mb-6">WE OFFER BEST & GENUINE EXPERIENCE</p> */}
           {/* <div className="w-24 h-1 bg-white mb-12"></div> */}
 
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
         
 
            <a href="https://wa.me/918740881801">
