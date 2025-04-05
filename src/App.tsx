073dcc10538c82e933ca7ba388db2ef12e612ac1
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-04-05 14:28:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
       <section 
         className="relative h-screen bg-cover bg-center"
         style={{
-          backgroundImage: 'url("abc.jpg")', // Correct path for public folder
+          backgroundImage: 'url("ab.webp")', // Correct path for public folder
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
